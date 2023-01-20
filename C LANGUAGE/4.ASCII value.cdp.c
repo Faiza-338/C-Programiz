94a60453cff21eee23cc06dc0c9b05d25d7c8b49
Faiza Khandoker <109524293+Faiza-338@users.noreply.github.com>
2023-01-20 16:57:48 +0600

    Add files via upload

---
+++ C LANGUAGE/4.ASCII value.cdp.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   char a;
+
+    printf("Enter a character:\n");
+    scanf("%c",&a);
+
+    printf("ASCII value of %c=%d",a,a);
+
+    return 0;
+}
