1ab5e20da5d7747b4a47d6e5c1d7696eb5f23ce3
Faiza Khandoker <109524293+Faiza-338@users.noreply.github.com>
2023-01-20 16:57:00 +0600

    Add files via upload

---
+++ C LANGUAGE/2.Add two integers.cdp.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,add;
+
+    printf("Enter two number:\n");
+    scanf("%d %d",&a,&b);
+
+    add=a+b;
+    printf("%d + %d = %d",a,b,add);
+
+    return 0;
+}
